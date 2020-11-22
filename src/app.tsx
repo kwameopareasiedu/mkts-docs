389972be1ba19f54dc5290d74695d69e504e9eab
Kwame Opare Asiedu <kwasiedu.5@gmail.com>
2020-11-22 11:15:18 +0000

    Moved coding comparison to its own widget

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Topbar } from "./topbar";
-import Coffee from "./assets/latte.svg";
-import Enter from "./assets/enter.svg";
+import { ComparisonSection } from "./comparison-section";
 import "./app.scss";
 
 export const App = (): any => {
@@ -13,7 +12,9 @@ export const App = (): any => {
                 <div className="container">
                     <blockquote className="mb-5">
                         <p>mkts is a tool that takes you from an empty folder to a running Typescript application base in literal seconds</p>
-                        <p>or minutes, if your internet connection is slow <i className="fa fa-laugh"/></p>
+                        <p>
+                            or minutes, if your internet connection is slow <i className="fa fa-laugh" />
+                        </p>
                     </blockquote>
 
                     <div id="install">
@@ -24,65 +25,7 @@ export const App = (): any => {
                 </div>
             </section>
 
-            <section id="comparison-section">
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-12 col-md-6">
-                            <div id="current-coding">
-                                <ul className="mb-5">
-                                    <li>Create project</li>
-                                    <li>Setup NPM</li>
-                                    <li>Install and configure Typescript</li>
-                                    <li>Install and configure ESlint</li>
-                                    <li>Install and configure Prettier</li>
-                                    <li>Install and configure Babel</li>
-                                    <li>Install Webpack</li>
-                                    <li>Configure Webpack to compile React TSX/JSX</li>
-                                    <li>Configure Webpack to compile SASS and CSS</li>
-                                    <li>Configure Webpack to link assets</li>
-
-                                    <p id="some-time-later">One eternity later...</p>
-
-                                    <li>Install node modules</li>
-                                </ul>
-
-                                <p className="coding-style mb-0">
-                                    How you <em>are</em> creating projects <i className="far fa-frown" />
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="col-12 col-md-6">
-                            <div id="new-coding">
-                                <div id="new-coding-content">
-                                    <ul>
-                                        <li>Open terminal at project location</li>
-                                        <li>
-                                            Run <code>mkts</code>
-                                        </li>
-                                        <li>Enter project name</li>
-                                        <li>Select project type</li>
-                                        <li>Select package manager</li>
-                                    </ul>
-
-                                    <div id="enter-btn">
-                                        <img src={Enter} alt="Enter key" />
-                                    </div>
-
-                                    <div id="coffee-break" className="mb-4">
-                                        <img src={Coffee} alt="coffee" />
-                                        <span>Sips coffee &amp; and watches magic happens</span>
-                                    </div>
-
-                                    <p className="coding-style mb-0">
-                                        How you <em>can be</em> creating projects <i className="far fa-laugh-beam" />
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <ComparisonSection />
         </div>
     );
 };
---
+++ src/comparison-section.tsx
@@ -0,0 +1,91 @@
+import React, { useRef, useState } from "react";
+import Enter from "./assets/enter.svg";
+import Coffee from "./assets/latte.svg";
+import "./comparison-section.scss";
+
+export const ComparisonSection = (): any => {
+    const sliderStartPosition = 65;
+    const [visibilitySliderValue, setVisibilitySliderValue] = useState(0);
+    const visibilitySliderRef = useRef();
+    const dividerRef = useRef();
+
+    const clipDistance = (): number => {
+        const visibilitySlider: HTMLInputElement = visibilitySliderRef.current;
+
+        if (visibilitySlider) {
+            const { width } = visibilitySlider.getBoundingClientRect();
+            return sliderStartPosition + 0.01 * visibilitySliderValue * (width - 30);
+        }
+
+        return sliderStartPosition;
+    };
+
+    return (
+        <section id="comparison-section">
+            <div id="new-coding">
+                <p className="coding-style">
+                    How you <em>can be</em> creating projects <i className="far fa-laugh-beam" />
+                </p>
+
+                <ul>
+                    <li>Open terminal at project location</li>
+                    <li>
+                        Run <code>mkts</code>
+                    </li>
+                    <li>Enter project name</li>
+                    <li>Select project type</li>
+                    <li>Select package manager</li>
+                </ul>
+
+                <div id="enter-btn">
+                    <img src={Enter} alt="Enter key" />
+                </div>
+
+                <div id="coffee-break" className="mb-4">
+                    <img src={Coffee} alt="coffee" />
+                    <span>Sips coffee &amp; and watches magic happens</span>
+                </div>
+            </div>
+
+            <div id="current-coding" style={{ clipPath: `polygon(${clipDistance()}px 0, 100% 0, 100% 100%, ${clipDistance()}px 100%)` }}>
+                <p className="coding-style">
+                    How you <em>are</em> creating projects <i className="far fa-frown" />
+                </p>
+
+                <ul className="mb-5">
+                    <li>Create project</li>
+                    <li>Setup NPM</li>
+                    <li>Install and configure Typescript</li>
+                    <li>Install and configure ESlint</li>
+                    <li>Install and configure Prettier</li>
+                    <li>Install and configure Babel</li>
+                    <li>Install Webpack</li>
+                    <li>Configure Webpack to compile React TSX/JSX</li>
+                    <li>Configure Webpack to compile SASS and CSS</li>
+                    <li>Configure Webpack to link assets</li>
+
+                    <p id="some-time-later">One eternity later...</p>
+
+                    <li>Install node modules</li>
+                </ul>
+            </div>
+
+            <div id="coding-divider" ref={dividerRef} style={{ left: `${clipDistance() - 3}px` }} />
+
+            <input
+                type="range"
+                id="visibility-slider"
+                ref={visibilitySliderRef}
+                value={visibilitySliderValue}
+                onChange={e => setVisibilitySliderValue(parseFloat(e.target.value))}
+                step={0.01}
+                max={100}
+                min={0}
+            />
+
+            <div id="drag-slider-hint" style={{ opacity: visibilitySliderValue > 2 ? 0 : 1 }}>
+                Drag slider <span>&nbsp;</span> <i className="fa fa-arrows-alt-h" />
+            </div>
+        </section>
+    );
+};
