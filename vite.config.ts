481c3f6c0c2b14cb8601bd649882c5a97d5529e8
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2022-10-09 06:55:31 +0000

    Configured Vite to build in lib/docs directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/mkts/",
+  build: {
+    outDir: "../mkts-lib/docs/",
+    emptyOutDir: true
+  },
   server: {
     host: "127.0.0.1",
     port: 8001
